29dc961ee96f3bded6db9c988c9ceca8f065d2a4
haibao <hhb219@163.com>
2017-12-19 18:10:19 +0800

    seamless-rolling
    jQuery无缝滚动插件，ES6

---
+++ js/seamlessRolling.js
@@ -0,0 +1,58 @@
+/**
+ * @desc 无缝滚动插件
+ * @author haibao[http://www.hehaibao.com/]
+ */
+;($ => {
+    $.seamlessRolling = options => new seamlessRolling(options);
+    class seamlessRolling {
+        constructor(options) {
+            this.init(options);
+        }
+        init(options) {
+            //默认配置
+            this.config = $.extend(true, {}, {
+                el: '.scroll-list', //滚动列表DOM
+                speed: 50 //滚动速度，值越大越慢
+            }, options);
+
+            this.roll();
+        }
+        roll() {
+            const _self = this;
+            const $ulList = $(_self.config.el).find('ul');
+            const [sh, uh] = [$(_self.config.el).height(),  $ulList.height()]; //ul父级高度，ul高度
+            let timer = null;
+
+            const start = () => {
+                clearTimeout(timer);
+                timer = setTimeout(() => {
+                    if(uh <= sh) {
+                        //滚动到底之后，清除定时器，重头开始
+                        clearTimeout(timer);
+                    } else {
+                        $ulList.stop().animate({
+                            marginTop: '-=1'
+                        }, 10, () => {
+                            const _top = Math.abs(parseInt($ulList.css("margin-top")));
+                            if(_top >= uh) {
+                                $ulList.animate({"margin-top": 0}, 0);
+                            }
+                        });
+                        timer = setTimeout(start, _self.config.speed);
+                    }
+                }, _self.config.speed);
+            };
+
+            //自动开始滚动
+            start();
+
+            //鼠标悬浮 暂停，离开则继续
+            $ulList.mouseenter(() => {
+                clearTimeout(timer);
+            }).mouseleave(() => {
+                start();
+            });
+        }
+    }
+
+})(window.jQuery);
